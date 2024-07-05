6751713c9af4e5ffa4456c3d9eeb1b9ac83745a2
Cristian Vianey Cáceres Labrador <cristiancacereslabrador@gmail.com>
2024-07-05 20:47:06 +0000

    Save progress: 1270.1

--- bike-shop.sh
+++ bike-shop.sh
@@ -61,10 +61,12 @@ RENT_MENU() {
         CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone = '$PHONE_NUMBER'")
         #if customer doesn't exist
         if [[ -z $CUSTOMER_NAME ]]
-then
-  # get new customer name
-  # insert new customer
-fi
+        then
+          # get new customer name
+          echo -e "\nWhat's your name?"
+
+          # insert new customer
+        fi
 
 
       fi
